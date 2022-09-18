a145683e8932efd484ba5e265e023e04f6fcc567
Wildan Fadh <wildanfadh79@gmail.com>
2022-09-18 07:40:21 +0700

    update search screen

--- App.tsx
+++ App.tsx
@@ -3,61 +3,61 @@ import React from 'react';
 // import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NativeBaseProvider, extendTheme} from 'native-base';
 
-const theme = extendTheme({
-  fontConfig: {
-    Roboto: {
-      100: {
-        normal: 'Roboto-Light',
-        italic: 'Roboto-LightItalic',
-      },
-      200: {
-        normal: 'Roboto-Light',
-        italic: 'Roboto-LightItalic',
-      },
-      300: {
-        normal: 'Roboto-Light',
-        italic: 'Roboto-LightItalic',
-      },
-      400: {
-        normal: 'Roboto-Regular',
-        italic: 'Roboto-Italic',
-      },
-      500: {
-        normal: 'Roboto-Medium',
-      },
-      600: {
-        normal: 'Roboto-Medium',
-        italic: 'Roboto-MediumItalic',
-      },
-      // Add more variants
-      700: {
-        normal: 'Roboto-Bold',
-      },
-      800: {
-        normal: 'Roboto-Bold',
-        italic: 'Roboto-BoldItalic',
-      },
-      900: {
-        normal: 'Roboto-Bold',
-        italic: 'Roboto-BoldItalic',
-      },
-    },
-  },
+// const theme = extendTheme({
+//   fontConfig: {
+//     Roboto: {
+//       100: {
+//         normal: 'Roboto-Light',
+//         italic: 'Roboto-LightItalic',
+//       },
+//       200: {
+//         normal: 'Roboto-Light',
+//         italic: 'Roboto-LightItalic',
+//       },
+//       300: {
+//         normal: 'Roboto-Light',
+//         italic: 'Roboto-LightItalic',
+//       },
+//       400: {
+//         normal: 'Roboto-Regular',
+//         italic: 'Roboto-Italic',
+//       },
+//       500: {
+//         normal: 'Roboto-Medium',
+//       },
+//       600: {
+//         normal: 'Roboto-Medium',
+//         italic: 'Roboto-MediumItalic',
+//       },
+//       // Add more variants
+//       700: {
+//         normal: 'Roboto-Bold',
+//       },
+//       800: {
+//         normal: 'Roboto-Bold',
+//         italic: 'Roboto-BoldItalic',
+//       },
+//       900: {
+//         normal: 'Roboto-Bold',
+//         italic: 'Roboto-BoldItalic',
+//       },
+//     },
+//   },
 
-  // Make sure values below matches any of the keys in `fontConfig`
-  fonts: {
-    heading: 'Roboto',
-    body: 'Roboto',
-    mono: 'Roboto',
-  },
-});
+//   // Make sure values below matches any of the keys in `fontConfig`
+//   fonts: {
+//     heading: 'Roboto',
+//     body: 'Roboto',
+//     mono: 'Roboto',
+//   },
+// });
 
 // router
 import Navigation from './src/router';
 
 export default function App() {
   return (
-    <NativeBaseProvider theme={theme}>
+    <NativeBaseProvider>
       <Navigation />
     </NativeBaseProvider>
   );
--- src/screens/search/index.tsx
+++ src/screens/search/index.tsx
@@ -1,6 +1,6 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
-import {Box, Text, HStack} from 'native-base';
+import {Box, Text, HStack, VStack, FlatList, Input, Icon} from 'native-base';
 
 // icons
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -11,11 +11,28 @@ import {CustomHeader} from '../../components';
 
 export const SearchScreen = () => {
   return (
-    <Box backgroundColor="white" height="100%">
-      <Box mx={3}>
-        <Text>SearchScreen</Text>
+    <>
+      <Box py={3} px={3} backgroundColor="white">
+        <Input
+          type="text"
+          placeholder="Search"
+          borderRadius={30}
+          backgroundColor="muted.100"
+          InputRightElement={
+            <Icon
+              as={AntDesign}
+              name="search1"
+              size="md"
+              color="muted.500"
+              mr={4}
+            />
+          }
+        />
       </Box>
-    </Box>
+      <Box backgroundColor="white" height="100%">
+        <Box mx={3}>{/* <Text>SearchScreen</Text> */}</Box>
+      </Box>
+    </>
   );
 };
 
