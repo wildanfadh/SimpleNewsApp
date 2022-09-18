d21c70805c73a67c3e0ad9e385233a6b1cda79f7
Wildan Fadh <wildanfadh79@gmail.com>
2022-09-18 08:03:17 +0700

    update bookmark screen

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,12 +1,24 @@
 import {StyleSheet, View, Dimensions} from 'react-native';
 import * as React from 'react';
-import {Box, Stack, HStack, VStack, Text, Image, Heading} from 'native-base';
+import {
+  Box,
+  Stack,
+  HStack,
+  VStack,
+  Text,
+  Image,
+  Heading,
+  Menu,
+  Pressable,
+  HamburgerIcon,
+} from 'native-base';
 
 const WindowWidth = Dimensions.get('window').width;
 const WindowHeight = Dimensions.get('window').height;
 
 // icons
 import Entypo from 'react-native-vector-icons/Entypo';
+import Feather from 'react-native-vector-icons/Feather';
 
 export const NewsCard = props => {
   // console.log('props', props.item.title);
@@ -17,7 +29,7 @@ export const NewsCard = props => {
       alignItems="center"
       my={3}
       px={2}
-      width={WindowWidth}
+      width={WindowWidth * 0.97}
       height="150px"
       backgroundColor="white">
       <Image
@@ -36,12 +48,32 @@ export const NewsCard = props => {
         <Text fontSize={12} color="muted.500">
           By {props.item.author}
         </Text>
-        <HStack>
+        <HStack space={2} alignItems="center" justifyContent="space-between">
           <HStack space={1}>
             <Text color="blue.500">{props.item.tag}</Text>
             <Entypo name="dot-single" size={24} color="black" />
             <Text>{props.item.created}</Text>
           </HStack>
+          <Menu
+            w="190"
+            trigger={triggerProps => {
+              return (
+                <Pressable
+                  accessibilityLabel="More options menu"
+                  {...triggerProps}>
+                  <Entypo name="dots-three-horizontal" size={20} />
+                </Pressable>
+              );
+            }}>
+            <Menu.Item>
+              <Entypo name="share-alternative" size={20} color="black" />
+              Share
+            </Menu.Item>
+            <Menu.Item>
+              <Feather name="bookmark" size={24} color="black" />
+              Bookmark
+            </Menu.Item>
+          </Menu>
         </HStack>
       </VStack>
     </HStack>
--- src/screens/bookmark/index.tsx
+++ src/screens/bookmark/index.tsx
@@ -1,11 +1,113 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, View, TouchableOpacity, Dimensions} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Box,
+  Stack,
+  HStack,
+  VStack,
+  Text,
+  Image,
+  FlatList,
+  Badge,
+} from 'native-base';
+import {useNavigation} from '@react-navigation/native';
+
+const WindowWidth = Dimensions.get('window').width;
+const WindowHeight = Dimensions.get('window').height;
+
+// components
+import {NewsCard} from '../../components';
+
+// image
+import newsLogo1 from '../../assets/images/newsLogo1.png';
+
+// data example
+const tags = [
+  {
+    title: 'World',
+  },
+  {
+    title: 'Politics',
+  },
+  {
+    title: 'Entertiment',
+  },
+  {
+    title: 'Science',
+  },
+  {
+    title: 'Education',
+  },
+];
+
+const news = [
+  {
+    title: 'Russia Withdraws Portion Of Troops From Border WIth Ukraine',
+    thumbnail:
+      'https://cdn.pixabay.com/photo/2014/10/02/06/34/war-469503__340.jpg',
+    author: 'Maureen Jones',
+    tag: 'World',
+    created: '32 minute ago',
+    content: `When just 200 Western monarch butterflies arrived in the Pismo Beach Butterfly Grove from their northerly migration last year, park rangers feared the treasured insect would soon be gone forever.
+
+This year, however, volunteers tallied their numbers at over 100,000, a spectacular swarm of hope that traveled down from as far north as Canada to the spend the winter on the California coast.
+
+It’s expected that the monarch butterfly will be placed on the Endangered Species List soon, due to declines in both western and eastern monarch butterfly numbers. Genetically indistinguishable, they are separate merely for the fact that monarchs living and migrating east of the Rockies overwinter in Mexico, while those on the western side of the Rockies overwinter along California’s west coast.`,
+  },
+];
 
 export const BookMarkScreen = () => {
+  const [tagSelected, setTagSelected] = useState('World');
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>BookMarkScreen</Text>
-    </View>
+    <>
+      <Box
+        backgroundColor="white"
+        // width={WindowWidth * 0.9}
+        height="100%"
+        pt={10}
+        px={3}>
+        <HStack space={2} alignItems="center">
+          <Text fontSize="md" fontWeight="bold">
+            Bookmarked
+          </Text>
+        </HStack>
+        <FlatList
+          my={10}
+          maxH={50}
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
+          data={tags}
+          renderItem={({item}) => (
+            <TouchableOpacity onPress={() => setTagSelected(item.title)}>
+              <Badge
+                mx={2}
+                borderRadius={10}
+                colorScheme={tagSelected == item.title ? 'dark' : 'light'}>
+                {item.title}
+              </Badge>
+            </TouchableOpacity>
+          )}
+        />
+        {/* <NewsCard /> */}
+        <FlatList
+          // mb={10}
+          // height="100%"
+          // backgroundColor="orange.300"
+          showsVerticalScrollIndicator={false}
+          data={news}
+          renderItem={({item}) => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('DetailNews', item);
+              }}>
+              <NewsCard item={item} />
+            </TouchableOpacity>
+          )}
+        />
+      </Box>
+    </>
   );
 };
 
